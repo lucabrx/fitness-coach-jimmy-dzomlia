f9ab039c2963cfb70e0d0d2a78b43f242c82160f
Luka Brkovic <coding@lbrkovic.com>
2023-05-17 20:00:41 +0200

    code cleanup

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,11 +7,14 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+     colors: {
+        'heading': '#FEFEFE',
+        "text" : "#6A6A6A",
+        "bg" : "#030303",
+        "card" : "#141414",
+        "s-gradiant" : "#FC575E",
+        "e-gradiant" : "#F7512C",
+     }
     },
   },
   plugins: [],
